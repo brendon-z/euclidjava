4595abfc0a69b54c9790a778b2487475c983e459
brendon_z <brendon.zhao@gmail.com>
2022-09-16 02:10:26 +1000

    initial algorithms, can be improved later

---
+++ euclid.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class Euclid {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read in numbers via command line
+        System.out.println("Enter number one: ");
+        int inputNumber1 = Integer.parseInt((scanner.nextLine()));
+        
+        System.out.println("Enter number two: ");
+        int inputNumber2 = Integer.parseInt((scanner.nextLine()));
+
+        int gcdResult = new Euclid().getGCD(inputNumber1, inputNumber2);
+        System.out.println("The GCD is: " + gcdResult);
+
+        scanner.close();
+    }
+
+    public int getGCD(int x1, int x2) {
+        int remainder = divisionAlgo(x1, x2);
+        if (remainder == 0) {
+            return x2;
+        } else {
+            return getGCD(x2, remainder);
+        }
+    }
+
+    // Simple "subtract until you can't no more" division algorithm
+    public int divisionAlgo(int x1, int x2) {
+        int smallest = Math.min(x1, x2);
+        int remainder = Math.max(x1, x2);
+
+        while (remainder >= smallest) {
+            remainder -= smallest;
+        }
+
+        return remainder;
+    }
+}
